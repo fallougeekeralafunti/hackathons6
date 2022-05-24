16d32fd0f36c20433d0e4d3e3941a5c0ee8d19d0
Fallou TOURE <fallou.toure@uadb.edu.sn>
2022-05-24 16:04:32 +0200

    mod de devops

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hackathon S6</title>
+    <title>Hackathon S6 | depuis devops</title>
 </head>
 <body>
     <h1>Hackathon Semaine 6</h1>
